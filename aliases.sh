f68cfdd6006094324ca1b7e14dbebcb41aba47cd
TimelyToga <tblumberg39@gmail.com>
2016-05-23 15:23:12 -0700

    Fixed errors

--- aliases.sh
+++ aliases.sh
@@ -2,13 +2,13 @@
 
 echo 'Creating aliases...'
 
-git config --global alias.l log --oneline --graph
-git config --global alias.pr pull -r origin master
-git config --global alias.ca commit --amend
-git config --global alias.a add .
-git config --global alias.s status
-git config --global alias.pm push origin master
-git config --global alias.c commit -m
+git config --global alias.l 'log --oneline --graph'
+git config --global alias.pr 'pull -r origin master'
+git config --global alias.ca 'commit --amend'
+git config --global alias.a 'add .'
+git config --global alias.s 'status'
+git config --global alias.pm 'push origin master'
+git config --global alias.c 'commit -m'
 
 echo 'The following aliases have been created:
 
