a8f13a2eac87e0c6cf495cfc6a90652a47ca9bcc
TimelyToga <tblumberg39@gmail.com>
2016-05-23 15:20:17 -0700

    created sh file

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,7 @@
+#!/bin/sh
+
+echo 'hey there'
+
 git config --global alias.l log --oneline --graph
 git config --global alias.pr pull -r origin master
 git config --global alias.ca commit --amend
@@ -5,3 +9,5 @@ git config --global alias.a add .
 git config --global alias.s status
 git config --global alias.pm push origin master
 git config --global alias.c commit -m
+
+echo ''
