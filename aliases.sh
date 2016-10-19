63c4e009223b885aebd853c3f3f370eb16e1fc86
TimelyToga <tblumberg39@gmail.com>
2016-10-19 06:34:59 -0400

    Added a bash function. Added heroku commands

--- aliases.sh
+++ aliases.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
-echo 'Creating aliases...'
+echo '\nCreating aliases...\n\n'
 
-git config --global alias.l 'log --oneline --graph'
-git config --global alias.pr 'pull -r origin master'
-git config --global alias.ca 'commit --amend'
-git config --global alias.a 'add .'
-git config --global alias.s 'status'
-git config --global alias.pm 'push origin master'
-git config --global alias.c 'commit -m'
+NUM=0
+createGitAlias(){
+	git config --global --replace-all alias.$1 "$2"
+	echo "$1 = $2"
+	let NUM=NUM+1
+}
 
-echo 'The following aliases have been created:
+createGitAlias a "add ."
+createGitAlias s "status"
+createGitAlias c "commit -m"
+createGitAlias l "log --oneline --graph"
+createGitAlias nb "checkout -b"
+createGitAlias pr "pull -r origin master"
+createGitAlias ca "commit --amend"
+createGitAlias pm "push origin master"
+createGitAlias fpm "push -f origin master"
+createGitAlias phm "push heroku master"
+createGitAlias fph "push -f heroku master"
 
-alias.l log --oneline --graph
-alias.pr pull -r origin master
-alias.ca commit --amend
-alias.a add .
-alias.s status
-alias.pm push origin master
-alias.c commit -m'
+echo '\n\n'$NUM 'aliases have been created. Enjoy the better life that I have given you.\n'
